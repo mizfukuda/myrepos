6e245d9d7d0b905abba688fe04af4648cfa2588c
Mizuho Fukuda <mfukuda@sigmaxyz.com>
2016-04-15 06:03:29 +0000

    char code debug

--- 02_speechAPI_request.py
+++ 02_speechAPI_request.py
@@ -12,7 +12,6 @@ clientId = "14c6e142-5175-4fec-b168-d9c9948d4fcc"
 clientSecret = "1cd81cba858147798dc63b993d88e727" 
 api = speech_api.speech_api(clientId, clientSecret)
 
-'''
 text = "This is a demo to call microsoft text to speach service in python. hogehoge!"
 content = api.text_to_speech(text, "en-US", True)
 retext = api.speech_to_text(content, "en-US", 8000, "ulm")
@@ -20,6 +19,7 @@ retext = api.speech_to_text(content, "en-US", 8000, "ulm")
 text = "roumaji de nyuuryoku suru to deru no kana. hoge hoge !"
 content = api.text_to_speech(text, "ja-JP", True)
 retext = api.speech_to_text(content, "ja-JP", 8000, "ulm")
+'''
 
 title = "Speech API sample"
 template = template.html_template()
